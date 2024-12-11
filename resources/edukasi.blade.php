a1abd87fd67023a290c52c2ea2febc56cb8631bd
unknown <Muhamamd Ilham Pratama>
2024-12-11 09:28:30 +0700

    Edukasi revisi

--- resources/edukasi.blade.php
+++ resources/edukasi.blade.php
@@ -14,8 +14,10 @@
 <!-- Semua Edukasi Start -->
 <div class="container-fluid blog background-index py-5">
     <div class="container pb-5">
-        <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px">
-            <h1 class="text-capitalize mb-3">Semua Edukasi</h1>
+        <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
+            <h1 class="text-capitalize mb-3">
+                Semua Edukasi
+            </h1>
         </div>
         <div class="row g-4 justify-content-center">
             @foreach($edukasis as $edukasi)
