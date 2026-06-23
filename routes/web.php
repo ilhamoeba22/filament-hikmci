b131a002c8ef8638ee7e07486da2d3828b070790
unknown <Muhamamd Ilham Pratama>
2026-06-23 10:11:15 +0700

    Add temporary route /run-user-setup

--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,46 @@ Route::get('/edukasi', [EdukasiController::class, 'index'])->name('edukasi.index
 Route::get('/edukasi/{id}', [EdukasiController::class, 'show'])->name('edukasi.show');
 
 Route::get('/ebook', [EbookController::class, 'index'])->name('ebook.index');
+
+// Route sementara untuk setup/update user Corsec dan Ilham
+Route::get('/run-user-setup', function () {
+    // 1. UPDATE/CREATE USER CORSEC
+    $corsec = \App\Models\User::where('email', 'corsec@gmail.com')
+                ->orWhere('email', 'corsec@email.com')
+                ->orWhere('email', 'admin@gmail.com')
+                ->first();
+
+    if ($corsec) {
+        $corsec->name = 'Corsec';
+        $corsec->email = 'corsec@gmail.com';
+        $corsec->password = \Illuminate\Support\Facades\Hash::make('Corsec@123');
+        $corsec->save();
+        echo "User Corsec berhasil diperbarui!<br>";
+    } else {
+        \App\Models\User::create([
+            'name' => 'Corsec',
+            'email' => 'corsec@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('Corsec@123'),
+        ]);
+        echo "User Corsec baru berhasil dibuat!<br>";
+    }
+
+    // 2. UPDATE/CREATE USER ILHAM
+    $ilham = \App\Models\User::where('email', 'ilham@gmail.com')->first();
+    if ($ilham) {
+        $ilham->name = 'Ilham';
+        $ilham->email = 'ilham@gmail.com';
+        $ilham->password = \Illuminate\Support\Facades\Hash::make('Muhammad@1234');
+        $ilham->save();
+        echo "User Ilham berhasil diperbarui!<br>";
+    } else {
+        \App\Models\User::create([
+            'name' => 'Ilham',
+            'email' => 'ilham@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('Muhammad@1234'),
+        ]);
+        echo "User Ilham baru berhasil dibuat!<br>";
+    }
+    
+    return "<br>Semua proses selesai! Silakan hapus kembali route ini demi keamanan.";
+});
